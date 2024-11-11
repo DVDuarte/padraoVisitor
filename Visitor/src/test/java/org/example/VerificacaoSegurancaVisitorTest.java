47bad463d5e611f999c5b5b5ee65c021a674a116
Daniel Duarte <danielvitasso@gmail.com>
2024-11-10 23:27:59 -0300

    caso de teste

--- Visitor/src/test/java/org/example/VerificacaoSegurancaVisitorTest.java
+++ Visitor/src/test/java/org/example/VerificacaoSegurancaVisitorTest.java
@@ -11,11 +11,10 @@ class VerificacaoSegurancaVisitorTest {
         Laptop laptop = new Laptop("usuarioLaptop");
         VerificacaoSegurancaVisitor visitor = new VerificacaoSegurancaVisitor();
 
-        // Simulamos a verificação de permissões do laptop
+
         laptop.aceitar(visitor);
 
-        // Aqui, poderíamos validar se a verificação do laptop ocorreu corretamente.
-        // Como o método `aceitar` apenas imprime para o console, o assert seria um exemplo para fins de prática.
+
         assertNotNull(laptop.getUsuario());
         assertEquals("usuarioLaptop", laptop.getUsuario());
     }
@@ -25,10 +24,8 @@ class VerificacaoSegurancaVisitorTest {
         Celular celular = new Celular("11999998888");
         VerificacaoSegurancaVisitor visitor = new VerificacaoSegurancaVisitor();
 
-        // Simulamos a atualização do certificado de segurança do celular
         celular.aceitar(visitor);
 
-        // Validações similares para o celular
         assertNotNull(celular.getNumeroTelefone());
         assertEquals("11999998888", celular.getNumeroTelefone());
     }
@@ -38,10 +35,8 @@ class VerificacaoSegurancaVisitorTest {
         Roteador roteador = new Roteador("192.168.0.1");
         VerificacaoSegurancaVisitor visitor = new VerificacaoSegurancaVisitor();
 
-        // Simulamos a verificação da configuração de segurança do roteador
         roteador.aceitar(visitor);
 
-        // Validações específicas do roteador
         assertNotNull(roteador.getIp());
         assertEquals("192.168.0.1", roteador.getIp());
     }
